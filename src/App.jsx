feaa2f8c69de57ab93484931596f49b70c701f75
Soham Khedkar <sohamunderscore99@gmail.com>
2024-09-01 14:24:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import { useDispatch,useSelector } from 'react-redux'
+import authService from  "./appwrite/auth"
+import { login, logout } from './store/authSlice'
+import {Header, Footer} from './components'
+import { Outlet } from 'react-router-dom'
+
+
+import './App.css'
+
+function App() {
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+        .then((userData) => {
+            if (userData) {
+                dispatch(login(userData));
+            } else {
+                dispatch(logout());
+            }
+        })
+        .catch((error) => {
+            console.error("Error during authentication:", error);
+            // Handle error appropriately, e.g., display error message to user
+        })
+        .finally(() => setLoading(false));
+}, []);
+
+
+return !loading ? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header />
+      <main>
+       <Outlet />
+      </main>
+      <Footer />
+    </div>
+  </div>
+) : null
+}
+
+export default App
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <h1 className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400'>DEVblogs</h1>
+    </div>
+  )
+}
+
+export default Logo
